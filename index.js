f0e689e560b160f790169d2655ef1e4d6d3d3cb8
EduardMat <128369415+EduardMat@users.noreply.github.com>
2023-12-29 15:46:50 +0000

    index created

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ function showAnswer(response){
 
 let apiKey = "oe3107c03bbf1b061844a8c3d518t9b3";
 let context = "be polite and provide a short answer";
-let prompt = " who was the first female president?";
+let prompt = " Who was the first president ? ";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+ 
 
 console.log("Processing...");
 axios.get(apiUrl).then(showAnswer);
