78d9abef17cbed1dbba441fc3dd03cbbe3f49273
EduardMat <128369415+EduardMat@users.noreply.github.com>
2024-01-05 05:28:49 +0000

    previous codes inactivated

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function showAnswer(response){
-    alert(response.data.answer);
-}
+    //function showAnswer(response){
+    //alert(response.data.answer);
+//}
 
-let apiKey = "oe3107c03bbf1b061844a8c3d518t9b3";
-let context = "be polite and provide a short answer";
-let prompt = " Who was the first president ? ";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+//let apiKey = "oe3107c03bbf1b061844a8c3d518t9b3";
+//let context = "be polite and provide a short answer";
+//let prompt = " Who was the first president ? ";
+//let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
  
 
-console.log("Processing...");
-axios.get(apiUrl).then(showAnswer);
+//console.log("Processing...");
+//axios.get(apiUrl).then(showAnswer);
+
