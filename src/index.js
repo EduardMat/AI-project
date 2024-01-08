5b9333073f24c9e45dd8783d7a7f8ed1142c74fd
EduardMat <128369415+EduardMat@users.noreply.github.com>
2024-01-08 06:14:16 +0000

    added the basic form handling to js and added typehanding writting

--- src/index.js
+++ src/index.js
@@ -14,3 +14,22 @@
 
 //The above code is about the pop in alert with the prime president etc -->
 
+function generatePoem(event){
+    event.preventDefault();
+
+
+    new Typewriter("#poem", {
+      strings:"La vita est bella ",
+      autoStart: true,
+      cursor: null,
+      delay: 60,
+    });
+    
+
+}
+
+
+
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
