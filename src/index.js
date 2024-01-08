a9914b235ae1c1a3a60b788656c0be82a8ee1dc3
EduardMat <128369415+EduardMat@users.noreply.github.com>
2024-01-08 22:13:17 +0000

    axios imported, working on the AI response

--- src/index.js
+++ src/index.js
@@ -14,18 +14,27 @@
 
 //The above code is about the pop in alert with the prime president etc -->
 
+function displayPoem(response){
+ new Typewriter("#poem", {
+   strings:response.data.answer, 
+   autoStart: true,
+   cursor: null,
+   delay: 60,
+ });
+       
+}
+
+
 function generatePoem(event){
     event.preventDefault();
+let apiKey = "oe3107c03bbf1b061844a8c3d518t9b3";
+let context = "you're a romantic poem expert";
+let prompt = "tell me a short poem ";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+axios.get(apiUrl).then(displayPoem);
 
 
-    new Typewriter("#poem", {
-      strings:"La vita est bella ",
-      autoStart: true,
-      cursor: null,
-      delay: 60,
-    });
     
-
 }
 
 
